7a249b27cf85bf25c344588aac1409cb5b8a2018
Jose Hernandez <pepeh93@hotmail.es>
2020-11-29 21:47:10 +0100

    Upload image doesn't work

--- Gemfile
+++ Gemfile
@@ -55,3 +55,5 @@ gem 'simple_form'
 
 # Gemfile
 gem 'dotenv-rails', groups: [:development, :test]
+
+gem 'cloudinary', '~> 1.16.0'
---
+++ db/migrate/20201129203947_destroy_photo_from_article.rb
@@ -0,0 +1,5 @@
+class DestroyPhotoFromArticle < ActiveRecord::Migration[6.0]
+  def change
+    remove_column :articles, :photo
+  end
+end
